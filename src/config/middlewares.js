cc5b454b8d4ef51c2aad3bbba5900acfd61e8406
Raissan Chedid <raissan@gmail.com>
2022-02-14 01:40:04 -0300

    Add swagger docs

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -1,10 +1,13 @@
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const cors = require('cors');
 // const knexLogger = require('knex-logger');
 
-const cors = require('cors');
+const swaggerDocs = require('../../__docs__/swagger.json');
 
 module.exports = app => {
   app.use(bodyParser.json());
-  // app.use(knexLogger(app.db));
   app.use(cors({ origin: '*' }));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+  // app.use(knexLogger(app.db));
 };
